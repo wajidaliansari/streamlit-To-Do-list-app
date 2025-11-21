9a9cf57ae3eed0bfe6ce96fbd9a28622abae06f7
Wajid Ali Ansari <112543652+wajidaliansari@users.noreply.github.com>
2025-11-22 02:04:45 +0530

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# streamlit-To-Do-list-app
-A simple and interactive To-Do List application built using Streamlit.
---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("To-Do List")
+
+task =st.text_input("Enter yout task", " ")
+
+if st.button("Add Task"):
+    if task:
+        st.session_state["task_list"].append(task)
+
+if "task_list" not in st.session_state:
+    st.session_state["task_list"] = []
+
+for i, t in enumerate(st.session_state["task_list"]):
+    st.write(f"{i + 1}. {t}")
+
+for i, t in enumerate(st.session_state["task_list"]):
+   if st.checkbox(f"{i + 1}. t"):
+       st.session_state["task_list"].remove(t)
